bf1552ed96a807ceeed6a8b65a90fb421823b8af
Sondre <sondreum@gmail.com>
2024-04-15 23:28:33 +0200

    restructured repo

--- src/crawler/spiders/quotes_spider.py
+++ src/crawler/spiders/quotes_spider.py
@@ -6,16 +6,23 @@ from bs4 import BeautifulSoup
 import os
 
 ROOT_PATH = Path(__file__).parent.parent.parent
-DATA_PATH = ROOT_PATH / "crawler_data"
+DATA_PATH = ROOT_PATH.joinpath("crawler_data")
+
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
-    start_urls = []
-    visited = []
+    start_urls: list[str] = []
+    visited: list[str] = []
     max_depth = 2
 
     # allow injection of starting urls
-    def __init__(self, name: str | None = None, urls: list[str] = [], max_depth: int | None = None, **kwargs: Any):
+    def __init__(
+        self,
+        name: str | None = None,
+        urls: list[str] = [],
+        max_depth: int | None = None,
+        **kwargs: Any,
+    ):
         self.start_urls = urls
         if max_depth:
             self.max_depth = max_depth
@@ -30,26 +37,27 @@ class QuotesSpider(scrapy.Spider):
     def find_links(self, response):
         soup = BeautifulSoup(response.body, "html.parser")
         links = []
-        for link in soup.find_all('a'):
-            links.append(link.get('href'))
+        for link in soup.find_all("a"):
+            links.append(link.get("href"))
 
         return [link for link in links if str(link)[0] == "/" and link not in self.visited]
 
     def parse(self, response, depth=0):
         self.visited.append(response.url)
         page = response.url.split("/")[-2]
-        filename = f'{page}.html'
-        
+        filename = f"{page}.html"
+
         # save data
         (DATA_PATH / filename).write_bytes(response.body)
 
         if depth >= self.max_depth:
             # limit the depth of the crawler
             return
-        
+
         links = self.find_links(response)
         yield from response.follow_all(links, callback=self.parse, cb_kwargs={"depth": depth + 1})
         # self.log(f"Saved file {filename}")
+
 
 if __name__ == "__main__":
     # clean up data directory
@@ -58,5 +66,8 @@ if __name__ == "__main__":
         os.remove(f"crawler_data/{file}")
 
     process = CrawlerProcess()
-    process.crawl(QuotesSpider, urls=["https://uit.no/research/csg?p_document_id=837262&Baseurl=%2Fresearch%2F"])
+    process.crawl(
+        QuotesSpider,
+        urls=["https://uit.no/research/csg?p_document_id=837262&Baseurl=%2Fresearch%2F"],
+    )
     process.start()
