66b1a49807da0c83557d7f5d937331961d023dbb
Stian Alexander Solli <sso177@uit.no>
2024-04-23 15:00:22 +0200

    1881 API (#7)
    * Created simple function for calling the 1881 api, and writing search results to disk
    
    * Ignored data from 1881 api, added 1881 api function to run from processing.py (commented out)
    
    * changed path to use pathlib

---
+++ src/api/yellowpages.py
@@ -0,0 +1,63 @@
+
+import requests
+import json
+from pathlib import Path
+import os
+
+
+API_KEY = None
+PARENT_PATH = Path(__file__).parent
+
+""" Search for a company name in the 1881 API, and write the results to disk"""
+def get_yellowpages_data(company_name: str):
+
+    path: Path = Path(PARENT_PATH).joinpath("yellowpages_data")
+    path.mkdir(parents=True, exist_ok=True)
+
+    for file in path.iterdir():
+        file.unlink()
+
+    with open(PARENT_PATH / "1881_key.txt") as file:
+        API_KEY = file.read().strip()
+
+    headers = {'Cache-Control': 'no-cache',
+               'Ocp-Apim-Subscription-Key': API_KEY}
+    
+
+    # Company name needs to have "%20" instead of spaces
+    company_name = company_name.replace(" ", "%20")
+    URL = f"https://services.api1881.no/search/company?query={company_name}"
+
+    # Make a request for the company name
+    response = requests.get(URL, headers=headers)
+    if response.status_code != 200:
+        print(f"Error: {response.status_code}")
+        return False
+    
+    # Write the response to a file
+    data = response.json()
+    string_data = json.dumps(data, indent=4)
+    resultpath = path / f"{company_name}_search_result.json"
+    with resultpath.open("w") as file:
+        file.write(string_data)
+
+    
+    # Request info for top 3 results and write to file
+    first_3 = data['contacts'][:3]
+    for contact in first_3:
+        orgnum = contact['organizationNumber']
+        URL2 = f"https://services.api1881.no/lookup/organizationnumber/{orgnum}"
+        response = requests.get(URL2, headers=headers)
+        if response.status_code != 200:
+            print(f"Error: {response.status_code}")
+            
+        contact_data = response.json()
+        string_contact = json.dumps(contact_data, indent=4)
+
+        filepath = path / f"{contact['name']}.json"
+        with filepath.open("w") as file:
+            file.write(string_contact)
+
+    
+
+    return True
