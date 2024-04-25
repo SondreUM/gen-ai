21105d319056884fd7bf4172901d7dee6c201757
Nikolai Lorentzen <nikolai.lorentzen@gmail.com>
2024-04-25 21:58:50 +0200

    better structure of code

--- src/googling.py
+++ src/googling.py
@@ -3,23 +3,27 @@ from gpt import init_agent
 
 def parse_results(results: list[SearchResult], query: str, verbose: bool) -> list[str]:
     agent = init_agent()
-
     relevant = []
 
+    agent.invoke(f"""You are an investigator looking for information about a company and it's employees.
+                 You are specifically looking for information about the the company called {query}.""")
+
     for result in results:
-        # evaluate description
         description = result.description
 
-        response = agent.invoke(f"""You are an investigator looking for information on {query}.
-                                Do you think it is likely that a page with the following description would contain relevant information?
-                                Include a yes/no in your answer in addition to reasoning.\n {description}""").content
+        response = agent.invoke(f"""Do you think it is likely that a page with the following description would contain relevant information about {query}?
+                                Include a yes/no in your answer in addition to your reasoning.\n
+                                {description}""").content
         if "yes" in response.lower():
             relevant.append(result)
         elif verbose:
-            print(f"Discarding {result.title} because {response}\n")
-    if verbose:    
+            print(f"Discarding {result.url}")
+            print(f"Reasoning: {response}")
+            print(f"Description: {description}\n")
+    if verbose:
+        print("\nRelevant results:")    
         for result in relevant:
-            print(result)
+            print(f"{result}\n")
 
     return relevant
 
