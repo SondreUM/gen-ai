ecae32db8857610b31cdf269e239ffefa502f15d
Nikolai Lorentzen <nikolai.lorentzen@gmail.com>
2024-05-06 20:40:32 +0200

    swithed : for = on defining the user agent

--- src/crawler/settings.py
+++ src/crawler/settings.py
@@ -14,7 +14,7 @@ NEWSPIDER_MODULE = "crawler.spiders"
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-USER_AGENT: "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
+USER_AGENT = "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
