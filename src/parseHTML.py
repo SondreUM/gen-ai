73392fdc69081bf6f8e2dddbcd046cc32047c213
Stian Alexander Solli <stianasolli@hotmail.com>
2024-05-07 16:27:17 +0200

    Added another html_parser found on stackoverflow, should be able to parse all visible text on a page

--- src/parseHTML.py
+++ src/parseHTML.py
@@ -2,6 +2,9 @@ import config
 from pathlib import Path
 from langchain_community.document_transformers import BeautifulSoupTransformer
 from langchain.docstore.document import Document
+from bs4 import BeautifulSoup
+from bs4.element import Comment
+
 
 def parse_HTML():
     """Parse the data collected by the crawler"""
@@ -35,3 +38,48 @@ def parse_HTML():
         
         with open(f"{parsed_dir.joinpath(file.with_suffix('.md').name)}", "w", encoding="utf-8", errors="ignore") as f:
             f.write(docs_transformed[idx].page_content)
+
+
+
+def parse_HTML_v2():
+    """Parse the data collected by the crawler"""
+
+    crawler_dir: Path = Path(config.DATA_PATH).joinpath("crawler_data")
+    parsed_dir = crawler_dir.parent.joinpath("parsed_data_v2")
+    parsed_dir.mkdir(parents=True, exist_ok=True)
+
+    for file in parsed_dir.iterdir():
+        file.unlink()
+
+    document = []
+ 
+    for file in crawler_dir.iterdir():
+        if file.is_dir():
+            continue
+
+        with open(file, "r", encoding="utf-8", errors="ignore") as f:
+            data = f.read()
+            text = text_from_html(data)
+            document.append(text)
+
+    for idx, file in enumerate(crawler_dir.iterdir()):
+        if file.is_dir():
+            continue
+        
+        with open(f"{parsed_dir.joinpath(file.with_suffix('.md').name)}", "w", encoding="utf-8", errors="ignore") as f:
+            f.write(document[idx])
+
+
+def tag_visible(element):
+    if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
+        return False
+    if isinstance(element, Comment):
+        return False
+    return True
+
+
+def text_from_html(body):
+    soup = BeautifulSoup(body, 'html.parser')
+    texts = soup.findAll(text=True)
+    visible_texts = filter(tag_visible, texts)  
+    return u" ".join(t.strip() for t in visible_texts)
